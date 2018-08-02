ae312131007f0a5ebaf81a57d3f62ac2bbaecf27
vagrant <vagrant@homestead>
2018-08-02 11:43:11 +0000

    User can upload avatar

---
+++ app/Handlers/ImageUploadHandler.php
@@ -0,0 +1,51 @@
+<?php 
+namespace App\Handlers;
+use Image;
+class ImageUploadHandler{
+	
+	protected $allowed_ext = ["png", "jpg", "gif", 'jpeg'];
+
+
+	public function save($file,$folder,$file_prefix,$max_width=false){
+
+		//文件夹的名称
+		$folder_name = "uploads/images/$folder/".date("Ym/d",time());
+
+		//文件具体存储的物理路经
+		$upload_path = public_path().'/'.$folder_name;
+
+		//后缀名
+		$extention  = strtolower($file->getClientOriginalExtension()) ?:'png';	
+
+		//文件名
+		$filename = $file_prefix.'_'.time().'_'.str_random(10).'.'.$extention;
+
+		if(!in_array($extention, $this->allowed_ext)){
+
+			return false;
+		}
+
+		$file->move($upload_path,$filename );
+
+		if($max_width && $extention != 'gif'){
+
+			$this->reduceSize($upload_path.'/'.$filename,$max_width);
+		}
+
+		return ["path"=>config('app.url')."/$folder_name/$filename"];
+	}
+
+	public function reduceSize($file_path,$max_width){
+
+			$image = Image::make($file_path);
+
+			$image->resize($max_width,null,function($constraint){
+
+				 $constraint->aspectRatio();
+				 $constraint->upsize();
+			});
+
+			$image->save();
+	}
+
+}
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
+use Auth;
 
 class UsersController extends Controller
 {
@@ -18,8 +20,20 @@ class UsersController extends Controller
     	return view('users.edit',compact('user'));
     }  
 
-    public function update(UserRequest $request,User $user){
-		$user->update($request->all());
+    public function update(UserRequest $request,User $user,ImageUploadHandler $uploader){
+
+    	$data = $request->all();
+
+    	if($request->avatar){
+
+    		$result = $uploader->save($request->avatar,'avatar',$user->id,362);
+
+    		if($result){
+    			$data['avatar'] = $result['path'];
+    		}
+    	}
+
+		$user->update($data);
         return redirect()->route('users.show', $user->id)->with('success','更新资料成功！');
    }
 
