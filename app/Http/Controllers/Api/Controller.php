7802bdaebb6f49d94f428a1b75672cf3ee50f5a9
vagrant <vagrant@homestead>
2018-11-12 19:00:52 +0000

    locale

--- app/Http/Controllers/Api/Controller.php
+++ app/Http/Controllers/Api/Controller.php
@@ -9,4 +9,11 @@ use Dingo\Api\Routing\Helpers;
 class Controller extends BaseController
 {
     use Helpers;
+
+
+    public function errorResponse($statusCode,$message=null,$code=0){
+
+
+		throw new HttpException($statusCode, $message, null, [], $code);
+    }
 }
